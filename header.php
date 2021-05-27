2f7f4d3ac76710f95d96b1a76be11673fda09a14
ChrisK-0 <73487474+ChrisK-0@users.noreply.github.com>
2021-05-27 13:37:57 +0300

    Front page ID with get in header.php

--- header.php
+++ header.php
@@ -33,7 +33,8 @@
 
                 <p class="cover_paragraph">
                     <?php
-                        $the_query = new WP_Query( 'page_id=7' );
+                        $front_page_id = get_option('page_on_front');
+                        $the_query = new WP_Query( 'page_id='.$front_page_id );
                         while ($the_query -> have_posts()) : $the_query -> the_post(); 
                             the_field('cover_paragraph');
                         endwhile;
@@ -41,23 +42,18 @@
                 </p>
             </div>
 
+            <div class="cover_anchor">
             <?php 
             if ( get_field('cover_anchor') ) {
                 echo '
-            <div class="cover_anchor">
                 <a id="gotoAccordionAnchor">
                     <p><img class="cover_anchor-arrow" src="'.get_template_directory_uri().'/assets/imgs/goto_arrow_down.png">
                         '.get_field('cover_anchor').'
                     </p>
                 </a>
-            </div>
-                    ';
-            } else {
-                echo '
-            <div class="cover_anchor">
-            </div>
                     ';
             }
             ?>
+            </div>
     </div>
 </div>
